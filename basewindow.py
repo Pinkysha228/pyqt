67ec44c93d86bac11c39a7e8d0ebefd575aaaa4e
Pinkysha228 <oleksijborsulak@gmail.com>
2025-05-11 19:42:17 +0300

    Update basewindow.py

--- basewindow.py
+++ basewindow.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QMessageBox
 
 
 class BaseWindow(QWidget):
@@ -6,6 +6,20 @@ class BaseWindow(QWidget):
         super().__init__()
         self.setWindowTitle(header)
         self.setMinimumWidth(250)
-        self.setFixedSize(400, 300)
+        self.setFixedSize(800, 600)
         with open('style.css', 'r') as stylesheet:
             self.setStyleSheet(stylesheet.read())
+
+    @staticmethod
+    def show_modal(header: str, text: str, icon: int = 0):
+        msg = QMessageBox()
+        msg.setWindowTitle(header)
+        msg.setText(text)
+        match icon:
+            case 0: msg.setIcon(QMessageBox.Information)
+            case 1: msg.setIcon(QMessageBox.Warning)
+            case 2: msg.setIcon(QMessageBox.Critical)
+            case 3: msg.setIcon(QMessageBox.Question)
+            case _: raise Exception('ваще та можна лише від 0 до 3')
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec_()
